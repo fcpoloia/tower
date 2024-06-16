da4491cf1a9fd0a1af2309f6d1586a33ab2bb33a
fcpolonia <z21comma@proton.me>
2024-06-16 22:50:05 +0200

    Upgrade system
    Gives player chooice of upgrades on the end of a wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from lib import PlayerClasses, EnemyClasses
+from lib import PlayerClasses, EnemyClasses, upgrade
 import random
 import sys
 
@@ -10,7 +10,8 @@ WIDTH: int = 1000
 HEIGHT: int = 700
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
-state = 'running'
+running = True
+state = "fight"
 clock = pygame.time.Clock()
 
 # initialize player
@@ -26,85 +27,98 @@ enemy_group.add(EnemyClasses.Conscript([100, 50], (WIDTH // 2, HEIGHT // 2), WID
 sides = ["L", "R", "U", "D"]
 
 wave = 0
+str_card = upgrade.str_upgrade_card(150,300)
+cards = pygame.sprite.Group()
+cards.add(str_card)
 
-while state == 'running':
+while running:
     # input map
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             player.fire(bullet_group)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            M_POS = event.pos
+            if str_card.rect.collidepoint(M_POS):
+                state = "fight"
+                str_card.upgrd(player)
 
-    # collision detection
-    player_hit = pygame.sprite.spritecollide(player, enemy_group, False)
-    for i in player_hit:
-        player.HEALTH -= 1
-        enemy_group.remove(i)
+    if state == "fight":
+        # collision detection
+        player_hit = pygame.sprite.spritecollide(player, enemy_group, False)
+        for i in player_hit:
+            player.HEALTH -= 1
+            enemy_group.remove(i)
 
-    bullet_hits = pygame.sprite.groupcollide(bullet_group, enemy_group, False, False)
-    for bullet in bullet_hits:
-        for enemy in bullet_hits[bullet]:
-            enemy.HP -= bullet.DAMAGE
-        bullet_group.remove(bullet)
-
-    # check for enemies to kill off
-    if enemy_group.sprites():
-        for i in enemy_group.sprites():
-            if i.HP <= 0:
-                enemy_group.remove(i)
-    # spawn new enemy
-    else:
-        for i in range(wave):
-            enemy = random.choice(pos_enemies)
-            side = random.choice(sides)
-            if side == "L":
-                enemy_group.add(
-                    enemy(
-                        [50, random.randint(0, HEIGHT)], (WIDTH / 2, HEIGHT / 2), WIDTH / 2
-                    )
-                )
-            if side == "R":
-                enemy_group.add(
-                    enemy(
-                        [950, random.randint(0, HEIGHT)], (WIDTH / 2, HEIGHT / 2), WIDTH / 2
-                    )
-                )
-            if side == "U":
-                enemy_group.add(
-                    enemy(
-                        [random.randint(0, WIDTH), 50], (WIDTH / 2, HEIGHT / 2), WIDTH / 2
-                    )
-                )
-            if side == "D":
-                enemy_group.add(
-                    enemy(
-                        [random.randint(0, WIDTH), 650], (WIDTH / 2, HEIGHT / 2), WIDTH / 2
-                    )
-                )
-        wave += 1
-        
-
-    # check for bullets out of bounds
-    for bullet in bullet_group.sprites():
-        if (
-            bullet.rect.centerx < 0
-            or bullet.rect.centerx > WIDTH
-            or bullet.rect.centery < 0
-            or bullet.rect.centery > HEIGHT
-        ):
+        bullet_hits = pygame.sprite.groupcollide(bullet_group, enemy_group, False, False)
+        for bullet in bullet_hits:
+            for enemy in bullet_hits[bullet]:
+                enemy.HP -= bullet.DAMAGE
             bullet_group.remove(bullet)
 
-    # Update & draw
-    screen.fill((80, 80, 80))
+        # check for enemies to kill off
+        if enemy_group.sprites():
+            for i in enemy_group.sprites():
+                if i.HP <= 0:
+                    enemy_group.remove(i)
+        # spawn new enemy
+        else:
+            for i in range(wave):
+                enemy = random.choice(pos_enemies)
+                side = random.choice(sides)
+                if side == "L":
+                    enemy_group.add(
+                        enemy(
+                            [50, random.randint(0, HEIGHT)], (WIDTH / 2, HEIGHT / 2), WIDTH / 2
+                        )
+                    )
+                if side == "R":
+                    enemy_group.add(
+                        enemy(
+                            [950, random.randint(0, HEIGHT)], (WIDTH / 2, HEIGHT / 2), WIDTH / 2
+                        )
+                    )
+                if side == "U":
+                    enemy_group.add(
+                        enemy(
+                            [random.randint(0, WIDTH), 50], (WIDTH / 2, HEIGHT / 2), WIDTH / 2
+                        )
+                    )
+                if side == "D":
+                    enemy_group.add(
+                        enemy(
+                            [random.randint(0, WIDTH), 650], (WIDTH / 2, HEIGHT / 2), WIDTH / 2
+                        )
+                    )
+            wave += 1
+            state = "buff"
 
-    player.update()
-    player_group.draw(screen)
+        # check for bullets out of bounds
+        for bullet in bullet_group.sprites():
+            if (
+                bullet.rect.centerx < 0
+                or bullet.rect.centerx > WIDTH
+                or bullet.rect.centery < 0
+                or bullet.rect.centery > HEIGHT
+            ):
+                bullet_group.remove(bullet)
 
-    bullet_group.update()
-    bullet_group.draw(screen)
+        # Update & draw
+        screen.fill((80, 80, 80))
 
-    enemy_group.update()
-    enemy_group.draw(screen)
+        player.update()
+        player_group.draw(screen)
+
+        bullet_group.update()
+        bullet_group.draw(screen)
+
+        enemy_group.update()
+        enemy_group.draw(screen)
+
+    elif state == "buff":
+        cards.update()
+        cards.draw(screen)
 
     pygame.display.flip()
     clock.tick(60)
